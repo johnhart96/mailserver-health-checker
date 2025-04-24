1a9cdbb43e5dde10dc48f308afa7051533bd5d06
John Hart <john@johnhart96.co.uk>
2025-04-24 10:02:28 +0100

    Update the installer to allow for crontab execution

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 rm /tmp/composer-setup.php
 composer install
 nano health-check.php
+
+# Add a cron job to run health-check.php every hour
+(crontab -l 2>/dev/null; echo "0 * * * * /usr/bin/php $(pwd)/health-check.php >> $(pwd)/monitor.log 2>&1") | crontab -
